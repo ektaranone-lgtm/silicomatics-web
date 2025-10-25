9a02fe6f0736590c2d49c21146751c3e918f1dc0
Silicoinformatics <ektara.none@gmail.com>
2025-10-25 15:37:10 -0400

    update 404 page

--- silico-frontend/src/router/index.ts
+++ silico-frontend/src/router/index.ts
@@ -9,7 +9,7 @@ import ForgotPasswordView from '../views/ForgotPasswordView.vue'
 import ResetPasswordView from '../views/ResetPasswordView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
